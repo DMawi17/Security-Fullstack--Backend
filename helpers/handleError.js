af2f9b5ded4160a9ee1195864698a7b8097c0269
DMawi17 <75326045+DMawi17@users.noreply.github.com>
2021-10-20 12:47:09 +0200

    --amend

--- helpers/handleError.js
+++ helpers/handleError.js
@@ -1,11 +1,7 @@
 const handleErrors = (err) => {
     let errors = {};
 
-    if (err.code === 11000) {
-        errors.email = "Email already exists";
-        return errors;
-    }
-
+    if (err.code === 11000) errors.email = "Email already exists";
     if (err.message.includes("User validation failed")) {
         Object.values(err.errors).forEach(({ properties }) => {
             errors[properties.path] = properties.message;
